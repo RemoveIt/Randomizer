df5d71cc248c34ff89f6181b538c371f0bb2c0a2
Usuń To <RemoveIt@users.noreply.github.com>
2014-10-22 19:39:37 +0300

    Displaying nothing

--- Client/init.ts
+++ Client/init.ts
@@ -1,1 +1,9 @@
-﻿
+﻿window.onload = function () {
+    var socket = io.connect(window.location.href);
+
+    var canvas = <HTMLCanvasElement>document.getElementById("GameCanvas");
+    var renderer = PIXI.autoDetectRenderer(800, 600, canvas);
+    var stage = new PIXI.Stage(0);
+
+    renderer.render(stage);
+}
